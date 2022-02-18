8dc840b5cc0870686af627b5b63f707c4be55540
balaviper <balaviper555@gmail.com>
2022-02-18 20:21:59 +0530

    commit two

--- Practice/Test.java
+++ Practice/Test.java
@@ -2,7 +2,8 @@ package Practice;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Bala");
+        System.out.println("Bala Viper");
+        System.out.println("Bala Viper 22");
     }
 }
 

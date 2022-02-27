e8ab6d89212ad971c9832219c684beb6dd15f467
balaviper <balaviper555@gmail.com>
2022-02-27 10:33:03 +0530

    Arithmatic Operators

--- Day_two/Abc.java
+++ Day_two/Abc.java
@@ -4,7 +4,9 @@ public class Abc {
 
     public static void main(String[] args) {
         
-        char c = 'h';
+
+        //Swapping concept  
+        char c ='h';
         char s = 's';
         System.out.println("Before Swapping " + c +" "+ s);
         char temp = c;
---
+++ Day_two/Operators.java
@@ -0,0 +1,17 @@
+package Day_two;
+
+public class Operators {
+    public static void main(String[] args) {
+
+        int num1 = 200;
+        int num2 = 50;
+
+        System.out.println("Addition : " + (num1 + num2));
+        System.out.println("Sub : " + (num1 - num2));
+        System.out.println("Mul : " + (num1 * num2));
+        System.out.println("Divide : " + (num1 / num2));
+        System.out.println("Reminder : " + (num1 % num2));
+
+    }
+    
+}

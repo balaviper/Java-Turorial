252c1da3e5b7ba82e3e74b3089b6e8c70450947d
balaviper <balaviper555@gmail.com>
2022-02-27 10:58:31 +0530

    Locical

--- Day_two/Operators.java
+++ Day_two/Operators.java
@@ -1,17 +1,42 @@
 package Day_two;
 
+import java.util.Scanner;
+
 public class Operators {
     public static void main(String[] args) {
 
-        int num1 = 200;
-        int num2 = 50;
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Addition : " + (num1 + num2));
-        System.out.println("Sub : " + (num1 - num2));
-        System.out.println("Mul : " + (num1 * num2));
-        System.out.println("Divide : " + (num1 / num2));
-        System.out.println("Reminder : " + (num1 % num2));
+        // int num1 = 200;
+        // int num2 = 50;
 
+        // System.out.println("Addition : " + (num1 + num2));
+        // System.out.println("Sub : " + (num1 - num2));
+        // System.out.println("Mul : " + (num1 * num2));
+        // System.out.println("Divide : " + (num1 / num2));
+        // System.out.println("Reminder : " + (num1 % num2));
+
+        //Simple Intrest : simple ingtrest  = Principle * no of years * rate /100
+        // System.out.println("Enter principle amount");
+        // double p = sc.nextInt();
+        // System.out.println("Enter no of years ");
+        // int n = sc.nextInt();
+        // System.out.println("Enter rate of intrest");
+        // int r = sc.nextInt();
+
+        // double simple_intrest = (p*n*r)/100;
+        // System.out.println("Simple Intrest : " + simple_intrest);
+
+        int v1 = 34;
+        int v2 = 35;
+        System.out.println("Greater : " + (v1!=v2));
+
+        //Logical
+        boolean and = (v1!=v2) && (11>16);
+        boolean or = (v1==v2) || (11>16);
+
+        System.out.println("Logical And " + and );
+        System.out.println("Logical Or " + or );
     }
     
 }

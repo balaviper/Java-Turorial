9e66286771cafab4269945981b2fe3ea42aba229
balaviper <balaviper555@gmail.com>
2022-02-27 12:48:14 +0530

    Strings

--- Day_two/Conditional.java
+++ Day_two/Conditional.java
@@ -12,6 +12,7 @@ public class Conditional {
         arr1[0] = 11;
         arr1[1] = 12;
         arr1[4] = 15; //init
+
         System.out.println("Before");
         for (int i : arr) {
             System.out.println(i);
@@ -22,15 +23,30 @@ public class Conditional {
             System.out.println(i);
         }
 
+        //Strings
+        char[] ch={'j','a','v','a'};
+
+        String address = "sdfhbsdfij sudfbsidjfbih iusdbfisbdf";
+        String s = new String(ch);
+
+        String n1 = "Naveena";
+        System.out.println(n1.length());
+        n1 = "Bala";
+        System.out.println(n1.length());
+
+
+        // Task 1 : 
+        //Arithmatic operator: 
+        // A train starts from A and stops at B. Speed of the train is 130km/hr.
+        //In between stops between a and b is 5, and train stops 2 min in every stop.
+        //Distance between A and B is 780km. How much time it takes to reach B from A.
         
+        //Task 2 :
+        // Calculate Compound Intrest : 
 
 
 
 
-
-
-
-        
 
 
        

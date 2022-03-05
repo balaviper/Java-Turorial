9100cb725109e0498b6e1b5d258a653d2dc8b134
balaviper <balaviper555@gmail.com>
2022-03-05 11:06:10 +0530

    Conditional Statement

---
+++ WeekTwo/Conditional.java
@@ -0,0 +1,52 @@
+package WeekTwo;
+
+public class Conditional {
+
+    public static void main(String[] args) {
+        
+
+        //If statement 
+        int num1 = 13;
+        int num2 = 15;
+
+        if(num1<num2) {
+            System.out.print("Greater : " + num1);
+        }
+        else{ 
+            System.out.print("Greater : " + num2);
+        }
+
+
+        // if(statement){
+
+        //     if(statement2) 
+        //     {
+
+        //     }
+
+        //     else{
+
+        //     }
+
+        // }
+
+        // else{
+
+        // }
+
+        if(num1==num2){
+            //Statement 
+        }
+        else if(num1>num2){
+            //Statat
+        }
+        else if (){
+            
+        }
+        else{
+
+        }
+
+    }
+    
+}

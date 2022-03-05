c2d09a1acb8bf02f1895d6bbcc7e92e16211dbe5
balaviper <balaviper555@gmail.com>
2022-03-05 10:49:55 +0530

    String Buffer

--- WeekTwo/Strings.java
+++ WeekTwo/Strings.java
@@ -3,23 +3,34 @@ package WeekTwo;
 public class Strings {
 
     public static void main(String[] args) {
-       
-        String name = "Balaaaaaaaaaaa";
-        System.out.println(name.charAt(1)); //accesing by index no
-        System.out.println(name.substring(2));
-        System.out.println(name.substring(2,7)); // substring
-        System.out.println(name.equals("Bala"));
+       //********* Strings ********************
 
-        String n1 = "KAALA";
-        String n2 = "kaala";
-        System.out.println(n1.equalsIgnoreCase(n2));
+        // String name = "Balaaaaaaaaaaa";
+        // System.out.println(name.charAt(1)); //accesing by index no
+        // System.out.println(name.substring(2));
+        // System.out.println(name.substring(2,7)); // substring
+        // System.out.println(name.equals("Bala"));
 
-        n1 = n1.concat(n2); //joining two string
-        System.out.println(n1);
+        // String n1 = "KAALA";
+        // String n2 = "kaala";
+        // System.out.println(n1.equalsIgnoreCase(n2));
+
+        // n1 = n1.concat(n2); //joining two string
+        // System.out.println(n1.length());
     
+        //*********String Buffer ********************
 
+        StringBuffer sb = new StringBuffer();
+
+        sb.append("BalaAAAAAAAAAAAAAAA");
+        sb.append("BalaAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+        System.out.println("String "+ sb);
+        System.out.println("String "+ sb.capacity()); //(oldcap *2) +2
+        sb.replace(1, 7, "BBBBBb");
+        sb.insert(9, "Z");
+
+        //********String Builder***********/
         
-
     }
     
 }

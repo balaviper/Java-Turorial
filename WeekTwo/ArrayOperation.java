5d405c1803992e75a5ba299e0c5f743eaafd011f
balaviper <balaviper555@gmail.com>
2022-03-06 12:07:00 +0530

    array end

--- WeekTwo/ArrayOperation.java
+++ WeekTwo/ArrayOperation.java
@@ -14,15 +14,27 @@ public class ArrayOperation {
             start++; //3
             end--; //2
         }
-
         return arr;
+    }
+
+    public static int[] add_end(int[] oldarr, int newElement){
+        int newArr[] = new int[oldarr.length + 1];
+        for (int i = 0; i<oldarr.length; i++) {
+            newArr[i] = oldarr[i];
+        }
+        int last = newArr.length;
+        newArr[last -1] = newElement;
+        return newArr;
     }
 
 
     public static void main(String[] args) {
          
-        int arr1[] = {4,6,2,1,4,8}; // array is is always fixed size 
-        arr1 = reverse(arr1);
+        int arr1[] = {4,6,2,1,4,8}; // array is is always fixed size
+    
+        arr1 = add_end(arr1, 60);
+        
+        // arr1 = reverse(arr1);
         for (int i : arr1) {
             System.out.println(i);
         }

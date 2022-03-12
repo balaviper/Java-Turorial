18a91a9845251d93706806fa81fec26e3598011d
balaviper <balaviper555@gmail.com>
2022-03-12 19:41:54 +0530

    Exception Handling

---
+++ WeekThree/ExceptionHandling.java
@@ -0,0 +1,39 @@
+package WeekThree;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionHandling {
+
+
+    public static void ThrowIng(){
+        Scanner sc = new Scanner(System.in);
+        int val = sc.nextInt();
+        if(val<0){
+            throw new ArithmeticException("Negative values cannot be accepted");
+        }
+
+    }
+
+    public static void Exp(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the no of students");
+            try {
+                int no = sc.nextInt();
+            System.out.println("Wrrong formate of input");
+            } catch (Exception e) {
+                System.out.println("Final block");
+            }
+            sc.nextLine();
+        //
+            int value = sc.nextInt();
+    }
+    public static void main(String[] args) {
+        
+        ThrowIng();
+
+
+    }
+    
+}
